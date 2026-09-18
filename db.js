a805581fbb345689edf524a9deb0d31f654a33ba
Євгеній <tprzpb51@gmail.com>
2026-09-18 14:00:41 +0300

    Use Neon PostgreSQL database

--- db.js
+++ db.js
@@ -1,8 +1,18 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './database.sqlite'
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL не налаштовано');
+}
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  logging: console.log,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 module.exports = sequelize;
